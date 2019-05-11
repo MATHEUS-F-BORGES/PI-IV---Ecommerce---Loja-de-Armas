49610ccca02916bd7093f68f9c65b792e22eebdd
Victor Serafim <>
2019-05-11 14:51:34 -0300

    ExemplosSpringApplication Alterado.

--- Airsoftware/src/main/java/br/senac/tads/airsoftware/controller/ExemplosSpringApplication.java
+++ Airsoftware/src/main/java/br/senac/tads/airsoftware/controller/ExemplosSpringApplication.java
@@ -3,9 +3,11 @@ package br.senac.tads.airsoftware.controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan (basePackages = "br.senac.tads.airsoftware.model")
+@ComponentScan({"br"})
 public class ExemplosSpringApplication {
 
     public static void main(String[] args) {
